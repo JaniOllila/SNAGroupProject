206a408648d08861ec916f2e453637b92b2b57ff
Jupe <jpajukangas24@gmail.com>
2026-05-10 13:34:39 +0300

    Added some comments

--- main.py
+++ main.py
@@ -8,7 +8,13 @@ import networkx as nx
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.linear_model import LogisticRegression
 import matplotlib.pyplot as plt
+"""
+    Code for tasks 1-11
 
+    Here at beginning adding tables that contain weather station coordinataes to be used to calculate turbine
+    distances to them as wind dataset did not contain any.
+    All of the plots can be found in plots and figures folder
+"""
 stations = {
     "Hailuoto":(65, 24.7),
     "Kokkola":(63.8, 23.1),
@@ -18,7 +24,6 @@ stations = {
     "Olkiluoto":(61.1,21.3),
     "Hamina":(60.4,27),
     "Pori":(61.6,21.4),
-    #"Jyvaskyla":(62.4,25.7),
     "Kemi_ajos":(65.7,24.5)
 }
 
@@ -32,7 +37,6 @@ stations_dataset = {
     "Hamina":"Kotka_Rankki.csv",
     "Pori":"Pori_Tahkoluoto_harbour.csv",
     "Kemi_ajos":"Kemi_Ajos.csv"
-    #"Jyvaskyla":"Jyvaskyla_airport.csv",
 }
 id_to_stat = {
     "Hailuoto":"Hailuoto Marjaniemi",
@@ -43,7 +47,6 @@ id_to_stat = {
     "Olkiluoto":"Rauma Kylmäpihlaja",
     "Hamina":"Kotka Rankki",
     "Pori":"Pori Tahkoluoto harbour",
-    #Jyvaskyla":"Jyväskylä airport AWOS",
     "Kemi_ajos":"Kemi Ajos"
 }
 
@@ -66,7 +69,7 @@ def read_wind_dataset(key):
     return fmi.read_fml_file("Wind datasets/" + stations_dataset[key],stations_dataset[key])
 
 #-----------------Task 1-2-----------------------------------
-#read and filter data 
+#read and filter powerplant data to be used later
 data_turbine = pd.read_csv("global_power_plant_database.csv")
 df = pd.DataFrame(data_turbine)
 filtered_df = df[df['country_long'].str.contains('Finland')]
@@ -113,6 +116,8 @@ fields = ['location', 'latitude','longitude','station_id']
 #df_all_wind_tocsv.to_csv("csv_all_wind.csv", index=False)
 
 df_all_wind = pd.read_csv("csv_all_wind.csv")
+#print(df_all_wind.info())
+df_all_wind.describe().to_csv("info_of_wind.csv")
 
 #-----------------Task 4-----------------------------------
 
@@ -136,6 +141,8 @@ single_vector.drop("Observation station", axis=1, inplace=True)
 #single_vector.to_csv("results", index=False)
 
 #-----------------Task 5-----------------------------------
+# Calculatin summary statitics for each turbine and making on dataframe of 
+# the data also scaling the data using standarscaler
 
 def rolling_mean(path):
 
@@ -182,13 +189,10 @@ data2 = pd.concat(rolling_features_list)
 single_vector_rolling_derived = pd.concat([data, data2], axis=1)
 single_vector_rolling_derived_cp = single_vector_rolling_derived.loc[:, ~single_vector_rolling_derived.columns.duplicated()]
 
-#print(single_vector_rolling_derived)
+
 # drop non-numeric column and save it for later
 ids = single_vector_rolling_derived["Observation station"]
 ids = ids.loc[:, ~ids.columns.duplicated()]
-
-#print(type(ids["Observation station"]))
-#print(ids["Observation station"].shape)
 
 X = single_vector_rolling_derived.drop(columns=["Observation station"])
 
@@ -203,6 +207,7 @@ def scale_func(X_scale_this):
 X_scaled = scale_func(X)
 
 #-----------------Task 6-----------------------------------
+#risk score calculations using logistig regression model
 
 def risk_score_calc(scaled_data, y_list):
 
@@ -216,11 +221,13 @@ def risk_score_calc(scaled_data, y_list):
     return scaled_data
 
 y = (df_all_wind.groupby("Observation station")["Maximum gust speed [m/s]"].max() > 30).astype(int)
+
 X_scaled = risk_score_calc(X_scaled, y)
+#print(X_scaled["risk_score_model"].describe())
 
 #-----------------Task 7-----------------------------------
-#Bringing X_scaled and turbine locations together first
-
+# Bringing X_scaled and turbine locations together first
+# Creates network by conecting nodes based on distance treshold
 X_scaled["Observation station"] = ids["Observation station"].values
 
 iter = turbines_df
@@ -267,15 +274,12 @@ for i, row1 in df_combined.iterrows():
                     weight=1
                 )
         
-
-
-
 #score_dict = dict(G.nodes(data="risk_sc"))
 #for node in score_dict:
 #    print(score_dict[node])
 
 #-----------------Task 8-----------------------------------
-
+#created similarity matrix and assing weight to each edge with some simple expontial function discounting distance score
 df_test = df_combined.drop(df_combined.columns.difference(["Wind speed [m/s]_mean",  "Wind speed [m/s]_std",  "Maximum gust speed [m/s]_max",  "rolling_std_3h","rolling_mean_3h"]), axis=1)
 
 similarity_matrix = cosine_similarity(df_test)
@@ -303,13 +307,6 @@ for i in range(len(df)):
 
         if(G.has_edge(df.iloc[i]["location"],df.iloc[j]["location"])):
             G[df.iloc[i]["location"]][df.iloc[j]["location"]]["weight"] = weight1
-        #elif(weight1>0.6):
-        #else:
-        #    G.add_edge(df.iloc[i]["location"],df.iloc[j]["location"],weight=weight1)
-
-#For edge weights
-#for u, v, d in G.edges(data=True):
-#    print(u, v, d["weight"])
 
 
 
@@ -333,17 +330,14 @@ nx.draw_networkx_labels(G, pos, font_size=10)
 
 nx.draw_networkx_edge_labels(G, pos, edge_labels=labels_round)
 plt.tight_layout()
-#nx.draw_networkx_edge_labels(G,pos,edge_labels=labels,connectionstyle="arc3")
 plt.savefig("plots_and_fiqures/network.png")
 #plt.show()
 
 nx.write_gexf(G, "graph.gexf")
 #-----------------Task 9-----------------------------------
-
+# Network metrics calculated.
 degrees = dict(G.degree())
 
-#biggest_node = max(degrees, key=degrees.get)
-#print("Highest degree node:", biggest_node, "Degree:", degrees[biggest_node])
 df_degree = pd.DataFrame({
     "location": list(degrees.keys()),
     "degree_value": list(degrees.values())
@@ -359,16 +353,15 @@ degree_values = list(degrees.values())
 avg_clust = nx.average_clustering(G, weight="weight")
 connect_comp = nx.number_connected_components(G)
 diameter = nx.diameter(G)
-print(connect_comp)
+
 
 d = {"avg_clust":avg_clust,"number_of_con_comp":connect_comp,"diameter":diameter,"edges":G.number_of_edges(),"nodes":G.number_of_nodes()}
 
 df_network_metrics = pd.DataFrame(data=d, index=[0])
 df_network_metrics.to_csv("network_metrics.csv")
-#the data is not in dataframes yet might do it but seem unnessesary
 
 #-----------------Task 10-----------------------------------
-
+# Identifying critical nodes in network based on nodes centrality measures
 deg_cent = nx.degree_centrality(G)
 
 bet_cent = nx.betweenness_centrality(G, weight="weight")
@@ -393,23 +386,22 @@ critical = df_analysis[
     (df_analysis["risk_score_model"] > df_analysis["risk_score_model"].mean()) & (df_analysis["betweenness_centrality"] > df_analysis["betweenness_centrality"].mean())
 ]
 
-#for _, row in df_analysis.iterrows():
-#    G[row["location"]]["betweenes"] = row["betweenness_centrality"]
-
 nx.set_node_attributes(G, bet_cent, "betweenness")
 nx.set_node_attributes(G, deg_cent, "degree_centrality")
 nx.set_node_attributes(G, clo_cent, "closeness_centrality")
 
-print(G.number_of_nodes())
-print(G.number_of_edges())
-print(G.nodes(data=True))
+#print(G.number_of_nodes())
+#print(G.number_of_edges())
+#print(G.nodes(data=True))
 
 
-print("critical locations in network: ")
-print(critical)
+#print("critical locations in network: ")
+#print(critical)
 color_map = []
 for node in G:
     if node in critical["location"].to_list():
+        color_map.append("red")
+    elif node == "Kokkola tv 1 ja 2":
         color_map.append("red")
     else:
         color_map.append("steelblue")
@@ -431,7 +423,7 @@ network_node_metrics = pd.DataFrame.from_dict(dict(G.nodes(data=True)), orient='
 network_node_metrics = network_node_metrics.drop(columns=["lat","long"]).to_csv("network_node_metrics.csv")
 
 #-----------------Task 11-----------------------------------
-
+# wind trends vs risk score calculating and grouping data for some period and plotting it and risk score changes
 def month_avg(key, all_wind=0):
     if(all_wind == 0):
         df = pd.read_csv("Parsed_wind datasets/"+key)
@@ -500,7 +492,6 @@ for key in stations_dataset:
     list_dfs2.append(risk_score_df_monthly)
     
 df_month_risk_score_each_station = pd.concat(list_dfs)
-#print(df_month_risk_score_each_station)
 
 plt.figure(figsize=(12,7))
 plt.subplot(1,2,1)
@@ -531,13 +522,11 @@ y = (all_stations_month_avg["Maximum gust speed [m/s]"] > (all_stations_month_av
 month_cp = all_stations_month_avg.drop(["Maximum wind speed [m/s]"],axis=1)
 df_scaled = scale_func(month_cp)
 risk_score_df_monthly = risk_score_calc(df_scaled,y)
-#print(risk_score_df_monthly)
+
 
 values_month_avg = all_stations_month_avg["Maximum gust speed [m/s]"].to_list()
 
 df_day_avgs = day_avg("location",1)
-#print(df_day_avgs)
-#print(df_day_avgs.info())
 
 plt.figure(figsize=(12,7))
 plt.subplot(1,2,1)
@@ -553,7 +542,6 @@ plt.xlabel("month")
 plt.ylabel("Risk_score")
 plt.title("Monthly risk_score")
 plt.savefig("plots_and_fiqures/Month_based_risk_colloctive_sc.png")
-#plt.show()
 
 plt.figure(figsize=(10,7))
 
@@ -566,7 +554,7 @@ plt.title("risk_score for each station")
 plt.xticks(rotation = 90)
 plt.tight_layout()
 plt.savefig("plots_and_fiqures/Risk_score_of_each_stat.png")
-#plt.show()
+
 
 
 y = (df_day_avgs["Wind speed [m/s]"] > (df_day_avgs["Wind speed [m/s]"].mean())).astype(int)
@@ -578,11 +566,7 @@ risk_score_df_day = risk_score_calc(df_scaled,y)
 plt.figure(figsize=(10,5))
 plt.subplot(1,2,1)
 plt.plot(df_day_avgs.index, df_day_avgs["Wind speed [m/s]"], label="Mean Wind")
-#print(df_day_avgs['Wind speed [m/s]_std'])
-#lower = df_day_avgs["Wind speed [m/s]"] - df_day_avgs['Wind speed [m/s]_std']
-#upper = df_day_avgs["Wind speed [m/s]"] + df_day_avgs['Wind speed [m/s]_std']
 
-#plt.fill_between(df_day_avgs.index, lower, upper, color='gray', alpha=0.3)
 plt.legend()
 plt.title("Wind Trends during one day")
 plt.xlabel("Hours")
@@ -603,7 +587,6 @@ df_day_avgs_year = day_avg_year("location",1)
 
 y = (df_day_avgs_year.groupby("day")["Maximum gust speed [m/s]"].max() > 9).astype(int)
 
-#y = (df_day_avgs_year["Wind speed [m/s]"] > (df_day_avgs_year["Wind speed [m/s]"].mean())).astype(int)
 day_cp = df_day_avgs_year.drop(["Maximum wind speed [m/s]"],axis=1)
 df_scaled = scale_func(day_cp)
 risk_score_df_day_year = risk_score_calc(df_scaled,y)
@@ -633,17 +616,13 @@ plt.xticks(rotation=45)
 plt.subplot(1,2,2)
 plt.plot(risk_score_df_day_year.index, risk_score_df_day_year["rolling_mean_5"], label="score")
 plt.legend()
-plt.title("Risk score Trends during day")
+plt.title("Risk score Trends during year")
 plt.xlabel("days")
 plt.ylabel("score")
 plt.xticks(rotation=45)
 plt.tight_layout()
 #plt.show()
 plt.savefig("plots_and_fiqures/day_wind_vs_score_year.png")
-
-
-#tulos = df_all_wind[df_all_wind["Maximum gust speed [m/s]"] > 25]
-#print(tulos)
 
 
 #-----------------Task 12-----------------------------------
